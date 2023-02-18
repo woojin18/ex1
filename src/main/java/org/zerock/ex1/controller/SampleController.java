7ae1499751e2ae37604c87debf2f19fdbea4927b
DESKTOP-SR1HUEI\wooji <woojin180213@naver.com>
2023-02-18 16:09:01 +0900

    test

--- src/main/java/org/zerock/ex1/controller/SampleController.java
+++ src/main/java/org/zerock/ex1/controller/SampleController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
     @GetMapping("/hello")
     public String[] hello(){
-        return new String[]{"Hello","World"};
+        return new String[]{"Hello","World11"};
     }
 }
